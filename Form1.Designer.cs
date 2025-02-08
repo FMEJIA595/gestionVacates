065cd858b6167faf7850dea8208110dbd53291c0
fmejia595 <fmejia595@unab.edu.co>
2025-02-08 16:10:23 -0500

    Commit inicial: Agregando proyecto y .gitignore

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace GestionVacantes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_EmpresasReclutamiento = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_DocumentoReclutamiento = new System.Windows.Forms.TextBox();
+            this.tb_NombreReclutamiento = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_CelularReclutamiento = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cb_CargosReclutamiento = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cb_FasesReclutamiento = new System.Windows.Forms.ComboBox();
+            this.dt_FechaReclutamiento = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button_RegistrarReclutamiento = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(129, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RECLUTAMIENTO";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cb_EmpresasReclutamiento
+            // 
+            this.cb_EmpresasReclutamiento.FormattingEnabled = true;
+            this.cb_EmpresasReclutamiento.Items.AddRange(new object[] {
+            "Marval",
+            "Cajasan"});
+            this.cb_EmpresasReclutamiento.Location = new System.Drawing.Point(132, 252);
+            this.cb_EmpresasReclutamiento.Name = "cb_EmpresasReclutamiento";
+            this.cb_EmpresasReclutamiento.Size = new System.Drawing.Size(124, 21);
+            this.cb_EmpresasReclutamiento.TabIndex = 1;
+            this.cb_EmpresasReclutamiento.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Empresa";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Documento";
+            // 
+            // tb_DocumentoReclutamiento
+            // 
+            this.tb_DocumentoReclutamiento.Location = new System.Drawing.Point(132, 87);
+            this.tb_DocumentoReclutamiento.Name = "tb_DocumentoReclutamiento";
+            this.tb_DocumentoReclutamiento.Size = new System.Drawing.Size(123, 20);
+            this.tb_DocumentoReclutamiento.TabIndex = 4;
+            // 
+            // tb_NombreReclutamiento
+            // 
+            this.tb_NombreReclutamiento.Location = new System.Drawing.Point(132, 124);
+            this.tb_NombreReclutamiento.Name = "tb_NombreReclutamiento";
+            this.tb_NombreReclutamiento.Size = new System.Drawing.Size(124, 20);
+            this.tb_NombreReclutamiento.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Nombre";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // tb_CelularReclutamiento
+            // 
+            this.tb_CelularReclutamiento.Location = new System.Drawing.Point(132, 161);
+            this.tb_CelularReclutamiento.Name = "tb_CelularReclutamiento";
+            this.tb_CelularReclutamiento.Size = new System.Drawing.Size(123, 20);
+            this.tb_CelularReclutamiento.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Celular";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Cargos";
+            // 
+            // cb_CargosReclutamiento
+            // 
+            this.cb_CargosReclutamiento.FormattingEnabled = true;
+            this.cb_CargosReclutamiento.Items.AddRange(new object[] {
+            "Asesor comercial",
+            "Ingeniero residente",
+            "Operador de maquinaria pesada",
+            "Agente comercial"});
+            this.cb_CargosReclutamiento.Location = new System.Drawing.Point(132, 205);
+            this.cb_CargosReclutamiento.Name = "cb_CargosReclutamiento";
+            this.cb_CargosReclutamiento.Size = new System.Drawing.Size(124, 21);
+            this.cb_CargosReclutamiento.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(38, 308);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Fases";
+            // 
+            // cb_FasesReclutamiento
+            // 
+            this.cb_FasesReclutamiento.FormattingEnabled = true;
+            this.cb_FasesReclutamiento.Items.AddRange(new object[] {
+            "Enviado",
+            "Contratado",
+            "Descartado",
+            "Desiste"});
+            this.cb_FasesReclutamiento.Location = new System.Drawing.Point(132, 300);
+            this.cb_FasesReclutamiento.Name = "cb_FasesReclutamiento";
+            this.cb_FasesReclutamiento.Size = new System.Drawing.Size(123, 21);
+            this.cb_FasesReclutamiento.TabIndex = 11;
+            // 
+            // dt_FechaReclutamiento
+            // 
+            this.dt_FechaReclutamiento.Location = new System.Drawing.Point(132, 346);
+            this.dt_FechaReclutamiento.Name = "dt_FechaReclutamiento";
+            this.dt_FechaReclutamiento.Size = new System.Drawing.Size(200, 20);
+            this.dt_FechaReclutamiento.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(42, 352);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Fecha";
+            // 
+            // button_RegistrarReclutamiento
+            // 
+            this.button_RegistrarReclutamiento.Location = new System.Drawing.Point(132, 415);
+            this.button_RegistrarReclutamiento.Name = "button_RegistrarReclutamiento";
+            this.button_RegistrarReclutamiento.Size = new System.Drawing.Size(75, 23);
+            this.button_RegistrarReclutamiento.TabIndex = 15;
+            this.button_RegistrarReclutamiento.Text = "Registrar";
+            this.button_RegistrarReclutamiento.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 453);
+            this.Controls.Add(this.button_RegistrarReclutamiento);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dt_FechaReclutamiento);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cb_FasesReclutamiento);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cb_CargosReclutamiento);
+            this.Controls.Add(this.tb_CelularReclutamiento);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tb_NombreReclutamiento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_DocumentoReclutamiento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cb_EmpresasReclutamiento);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb_EmpresasReclutamiento;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_DocumentoReclutamiento;
+        private System.Windows.Forms.TextBox tb_NombreReclutamiento;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_CelularReclutamiento;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cb_CargosReclutamiento;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cb_FasesReclutamiento;
+        private System.Windows.Forms.DateTimePicker dt_FechaReclutamiento;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button_RegistrarReclutamiento;
+    }
+}
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace GestionVacantes
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VACANTES = new System.Windows.Forms.Label();
+            this.cb_ResponsableVacantes = new System.Windows.Forms.ComboBox();
+            this.Responsable = new System.Windows.Forms.Label();
+            this.dt_FechaSolicitudVacantes = new System.Windows.Forms.DateTimePicker();
+            this.FechaSolicitudVacantes = new System.Windows.Forms.Label();
+            this.EmpresaVacantes = new System.Windows.Forms.Label();
+            this.cb_EmpresaVacante = new System.Windows.Forms.ComboBox();
+            this.CargoVacantes = new System.Windows.Forms.Label();
+            this.cb_CargoVacantes = new System.Windows.Forms.ComboBox();
+            this.n_NumeroVacantes = new System.Windows.Forms.NumericUpDown();
+            this.NumeroVacantes = new System.Windows.Forms.Label();
+            this.tb_PerfilVacantes = new System.Windows.Forms.TextBox();
+            this.Perfil = new System.Windows.Forms.Label();
+            this.button_RegistrarVacantes = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.n_NumeroVacantes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // VACANTES
+            // 
+            this.VACANTES.AutoSize = true;
+            this.VACANTES.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VACANTES.Location = new System.Drawing.Point(165, 21);
+            this.VACANTES.Name = "VACANTES";
+            this.VACANTES.Size = new System.Drawing.Size(99, 18);
+            this.VACANTES.TabIndex = 0;
+            this.VACANTES.Text = "VACANTES";
+            // 
+            // cb_ResponsableVacantes
+            // 
+            this.cb_ResponsableVacantes.FormattingEnabled = true;
+            this.cb_ResponsableVacantes.Items.AddRange(new object[] {
+            "Fabian Mejia",
+            "Tania Pinillos"});
+            this.cb_ResponsableVacantes.Location = new System.Drawing.Point(168, 74);
+            this.cb_ResponsableVacantes.Name = "cb_ResponsableVacantes";
+            this.cb_ResponsableVacantes.Size = new System.Drawing.Size(121, 21);
+            this.cb_ResponsableVacantes.TabIndex = 1;
+            // 
+            // Responsable
+            // 
+            this.Responsable.AutoSize = true;
+            this.Responsable.Location = new System.Drawing.Point(47, 82);
+            this.Responsable.Name = "Responsable";
+            this.Responsable.Size = new System.Drawing.Size(69, 13);
+            this.Responsable.TabIndex = 2;
+            this.Responsable.Text = "Responsable";
+            // 
+            // dt_FechaSolicitudVacantes
+            // 
+            this.dt_FechaSolicitudVacantes.Location = new System.Drawing.Point(168, 112);
+            this.dt_FechaSolicitudVacantes.Name = "dt_FechaSolicitudVacantes";
+            this.dt_FechaSolicitudVacantes.Size = new System.Drawing.Size(200, 20);
+            this.dt_FechaSolicitudVacantes.TabIndex = 3;
+            // 
+            // FechaSolicitudVacantes
+            // 
+            this.FechaSolicitudVacantes.AutoSize = true;
+            this.FechaSolicitudVacantes.Location = new System.Drawing.Point(47, 118);
+            this.FechaSolicitudVacantes.Name = "FechaSolicitudVacantes";
+            this.FechaSolicitudVacantes.Size = new System.Drawing.Size(95, 13);
+            this.FechaSolicitudVacantes.TabIndex = 4;
+            this.FechaSolicitudVacantes.Text = "Fecha de Solicitud";
+            // 
+            // EmpresaVacantes
+            // 
+            this.EmpresaVacantes.AutoSize = true;
+            this.EmpresaVacantes.Location = new System.Drawing.Point(47, 152);
+            this.EmpresaVacantes.Name = "EmpresaVacantes";
+            this.EmpresaVacantes.Size = new System.Drawing.Size(48, 13);
+            this.EmpresaVacantes.TabIndex = 6;
+            this.EmpresaVacantes.Text = "Empresa";
+            // 
+            // cb_EmpresaVacante
+            // 
+            this.cb_EmpresaVacante.FormattingEnabled = true;
+            this.cb_EmpresaVacante.Items.AddRange(new object[] {
+            "Fabian Mejia",
+            "Tania Pinillos"});
+            this.cb_EmpresaVacante.Location = new System.Drawing.Point(168, 149);
+            this.cb_EmpresaVacante.Name = "cb_EmpresaVacante";
+            this.cb_EmpresaVacante.Size = new System.Drawing.Size(121, 21);
+            this.cb_EmpresaVacante.TabIndex = 5;
+            // 
+            // CargoVacantes
+            // 
+            this.CargoVacantes.AutoSize = true;
+            this.CargoVacantes.Location = new System.Drawing.Point(47, 191);
+            this.CargoVacantes.Name = "CargoVacantes";
+            this.CargoVacantes.Size = new System.Drawing.Size(35, 13);
+            this.CargoVacantes.TabIndex = 8;
+            this.CargoVacantes.Text = "Cargo";
+            // 
+            // cb_CargoVacantes
+            // 
+            this.cb_CargoVacantes.FormattingEnabled = true;
+            this.cb_CargoVacantes.Items.AddRange(new object[] {
+            "Fabian Mejia",
+            "Tania Pinillos"});
+            this.cb_CargoVacantes.Location = new System.Drawing.Point(168, 188);
+            this.cb_CargoVacantes.Name = "cb_CargoVacantes";
+            this.cb_CargoVacantes.Size = new System.Drawing.Size(121, 21);
+            this.cb_CargoVacantes.TabIndex = 7;
+            // 
+            // n_NumeroVacantes
+            // 
+            this.n_NumeroVacantes.Location = new System.Drawing.Point(168, 230);
+            this.n_NumeroVacantes.Name = "n_NumeroVacantes";
+            this.n_NumeroVacantes.Size = new System.Drawing.Size(120, 20);
+            this.n_NumeroVacantes.TabIndex = 9;
+            // 
+            // NumeroVacantes
+            // 
+            this.NumeroVacantes.AutoSize = true;
+            this.NumeroVacantes.Location = new System.Drawing.Point(50, 236);
+            this.NumeroVacantes.Name = "NumeroVacantes";
+            this.NumeroVacantes.Size = new System.Drawing.Size(106, 13);
+            this.NumeroVacantes.TabIndex = 10;
+            this.NumeroVacantes.Text = "Número de vacantes";
+            // 
+            // tb_PerfilVacantes
+            // 
+            this.tb_PerfilVacantes.Location = new System.Drawing.Point(168, 276);
+            this.tb_PerfilVacantes.Name = "tb_PerfilVacantes";
+            this.tb_PerfilVacantes.Size = new System.Drawing.Size(100, 20);
+            this.tb_PerfilVacantes.TabIndex = 11;
+            // 
+            // Perfil
+            // 
+            this.Perfil.AutoSize = true;
+            this.Perfil.Location = new System.Drawing.Point(53, 282);
+            this.Perfil.Name = "Perfil";
+            this.Perfil.Size = new System.Drawing.Size(32, 13);
+            this.Perfil.TabIndex = 12;
+            this.Perfil.Text = "Perfíl";
+            // 
+            // button_RegistrarVacantes
+            // 
+            this.button_RegistrarVacantes.Location = new System.Drawing.Point(168, 347);
+            this.button_RegistrarVacantes.Name = "button_RegistrarVacantes";
+            this.button_RegistrarVacantes.Size = new System.Drawing.Size(75, 23);
+            this.button_RegistrarVacantes.TabIndex = 13;
+            this.button_RegistrarVacantes.Text = "Registrar";
+            this.button_RegistrarVacantes.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 450);
+            this.Controls.Add(this.button_RegistrarVacantes);
+            this.Controls.Add(this.Perfil);
+            this.Controls.Add(this.tb_PerfilVacantes);
+            this.Controls.Add(this.NumeroVacantes);
+            this.Controls.Add(this.n_NumeroVacantes);
+            this.Controls.Add(this.CargoVacantes);
+            this.Controls.Add(this.cb_CargoVacantes);
+            this.Controls.Add(this.EmpresaVacantes);
+            this.Controls.Add(this.cb_EmpresaVacante);
+            this.Controls.Add(this.FechaSolicitudVacantes);
+            this.Controls.Add(this.dt_FechaSolicitudVacantes);
+            this.Controls.Add(this.Responsable);
+            this.Controls.Add(this.cb_ResponsableVacantes);
+            this.Controls.Add(this.VACANTES);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.n_NumeroVacantes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label VACANTES;
+        private System.Windows.Forms.ComboBox cb_ResponsableVacantes;
+        private System.Windows.Forms.Label Responsable;
+        private System.Windows.Forms.DateTimePicker dt_FechaSolicitudVacantes;
+        private System.Windows.Forms.Label FechaSolicitudVacantes;
+        private System.Windows.Forms.Label EmpresaVacantes;
+        private System.Windows.Forms.ComboBox cb_EmpresaVacante;
+        private System.Windows.Forms.Label CargoVacantes;
+        private System.Windows.Forms.ComboBox cb_CargoVacantes;
+        private System.Windows.Forms.NumericUpDown n_NumeroVacantes;
+        private System.Windows.Forms.Label NumeroVacantes;
+        private System.Windows.Forms.TextBox tb_PerfilVacantes;
+        private System.Windows.Forms.Label Perfil;
+        private System.Windows.Forms.Button button_RegistrarVacantes;
+    }
+}
