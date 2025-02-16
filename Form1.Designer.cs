0122693bf74073c1becec6d055f68a448803a1f2
fmejia595 <fmejia595@unab.edu.co>
2025-02-16 15:24:07 -0500

    Actualizando .gitignore e ignorando .vs (corrección de permisos)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,6 +44,8 @@
             this.dt_FechaReclutamiento = new System.Windows.Forms.DateTimePicker();
             this.label8 = new System.Windows.Forms.Label();
             this.button_RegistrarReclutamiento = new System.Windows.Forms.Button();
+            this.tb_Comentarios = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -52,7 +54,7 @@
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(129, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 18);
+            this.label1.Size = new System.Drawing.Size(158, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "RECLUTAMIENTO";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -63,7 +65,7 @@
             this.cb_EmpresasReclutamiento.Items.AddRange(new object[] {
             "Marval",
             "Cajasan"});
-            this.cb_EmpresasReclutamiento.Location = new System.Drawing.Point(132, 252);
+            this.cb_EmpresasReclutamiento.Location = new System.Drawing.Point(133, 208);
             this.cb_EmpresasReclutamiento.Name = "cb_EmpresasReclutamiento";
             this.cb_EmpresasReclutamiento.Size = new System.Drawing.Size(124, 21);
             this.cb_EmpresasReclutamiento.TabIndex = 1;
@@ -72,7 +74,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(39, 260);
+            this.label2.Location = new System.Drawing.Point(40, 216);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 2;
@@ -82,7 +84,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(39, 95);
+            this.label3.Location = new System.Drawing.Point(40, 51);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(62, 13);
             this.label3.TabIndex = 3;
@@ -90,14 +92,14 @@
             // 
             // tb_DocumentoReclutamiento
             // 
-            this.tb_DocumentoReclutamiento.Location = new System.Drawing.Point(132, 87);
+            this.tb_DocumentoReclutamiento.Location = new System.Drawing.Point(133, 43);
             this.tb_DocumentoReclutamiento.Name = "tb_DocumentoReclutamiento";
             this.tb_DocumentoReclutamiento.Size = new System.Drawing.Size(123, 20);
             this.tb_DocumentoReclutamiento.TabIndex = 4;
             // 
             // tb_NombreReclutamiento
             // 
-            this.tb_NombreReclutamiento.Location = new System.Drawing.Point(132, 124);
+            this.tb_NombreReclutamiento.Location = new System.Drawing.Point(133, 80);
             this.tb_NombreReclutamiento.Name = "tb_NombreReclutamiento";
             this.tb_NombreReclutamiento.Size = new System.Drawing.Size(124, 20);
             this.tb_NombreReclutamiento.TabIndex = 6;
@@ -105,7 +107,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(39, 132);
+            this.label4.Location = new System.Drawing.Point(40, 88);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(44, 13);
             this.label4.TabIndex = 5;
@@ -114,7 +116,7 @@
             // 
             // tb_CelularReclutamiento
             // 
-            this.tb_CelularReclutamiento.Location = new System.Drawing.Point(132, 161);
+            this.tb_CelularReclutamiento.Location = new System.Drawing.Point(133, 117);
             this.tb_CelularReclutamiento.Name = "tb_CelularReclutamiento";
             this.tb_CelularReclutamiento.Size = new System.Drawing.Size(123, 20);
             this.tb_CelularReclutamiento.TabIndex = 8;
@@ -122,7 +124,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(39, 169);
+            this.label5.Location = new System.Drawing.Point(40, 125);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(39, 13);
             this.label5.TabIndex = 7;
@@ -132,7 +134,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(39, 213);
+            this.label6.Location = new System.Drawing.Point(40, 169);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(40, 13);
             this.label6.TabIndex = 10;
@@ -146,7 +148,7 @@
             "Ingeniero residente",
             "Operador de maquinaria pesada",
             "Agente comercial"});
-            this.cb_CargosReclutamiento.Location = new System.Drawing.Point(132, 205);
+            this.cb_CargosReclutamiento.Location = new System.Drawing.Point(133, 161);
             this.cb_CargosReclutamiento.Name = "cb_CargosReclutamiento";
             this.cb_CargosReclutamiento.Size = new System.Drawing.Size(124, 21);
             this.cb_CargosReclutamiento.TabIndex = 9;
@@ -154,7 +156,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(38, 308);
+            this.label7.Location = new System.Drawing.Point(39, 264);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(35, 13);
             this.label7.TabIndex = 12;
@@ -168,14 +170,14 @@
             "Contratado",
             "Descartado",
             "Desiste"});
-            this.cb_FasesReclutamiento.Location = new System.Drawing.Point(132, 300);
+            this.cb_FasesReclutamiento.Location = new System.Drawing.Point(133, 256);
             this.cb_FasesReclutamiento.Name = "cb_FasesReclutamiento";
             this.cb_FasesReclutamiento.Size = new System.Drawing.Size(123, 21);
             this.cb_FasesReclutamiento.TabIndex = 11;
             // 
             // dt_FechaReclutamiento
             // 
-            this.dt_FechaReclutamiento.Location = new System.Drawing.Point(132, 346);
+            this.dt_FechaReclutamiento.Location = new System.Drawing.Point(133, 302);
             this.dt_FechaReclutamiento.Name = "dt_FechaReclutamiento";
             this.dt_FechaReclutamiento.Size = new System.Drawing.Size(200, 20);
             this.dt_FechaReclutamiento.TabIndex = 13;
@@ -183,7 +185,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(42, 352);
+            this.label8.Location = new System.Drawing.Point(43, 308);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(37, 13);
             this.label8.TabIndex = 14;
@@ -198,11 +200,29 @@
             this.button_RegistrarReclutamiento.Text = "Registrar";
             this.button_RegistrarReclutamiento.UseVisualStyleBackColor = true;
             // 
+            // tb_Comentarios
+            // 
+            this.tb_Comentarios.AutoSize = true;
+            this.tb_Comentarios.Location = new System.Drawing.Point(43, 346);
+            this.tb_Comentarios.Name = "tb_Comentarios";
+            this.tb_Comentarios.Size = new System.Drawing.Size(65, 13);
+            this.tb_Comentarios.TabIndex = 16;
+            this.tb_Comentarios.Text = "Comentarios";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 338);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 17;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(402, 453);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tb_Comentarios);
             this.Controls.Add(this.button_RegistrarReclutamiento);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.dt_FechaReclutamiento);
@@ -245,6 +265,8 @@
         private System.Windows.Forms.DateTimePicker dt_FechaReclutamiento;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button button_RegistrarReclutamiento;
+        private System.Windows.Forms.Label tb_Comentarios;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
